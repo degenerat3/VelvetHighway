3d2f0c90e419d7ab611fd08f0b5e3fa8617fa394
degenerat3 <james.maskelony@gmail.com>
2019-12-30 17:33:00 -0500

    fix purchase format

--- site/purchase.php
+++ site/purchase.php
@@ -1,18 +1,24 @@
-<?php include "templates/header.php"; ?><h2>Add a user</h2>
+<?php include "templates/header.php"; ?><h3>Create a new purchase order</h3>
 
     <form method="post">
     	<label for="firstname">First Name</label>
     	<input type="text" name="firstname" id="firstname">
+        <br><br>
     	<label for="lastname">Last Name</label>
     	<input type="text" name="lastname" id="lastname">
+        <br><br>
     	<label for="email">Email Address</label>
     	<input type="text" name="email" id="email">
+        <br><br>
         <label for="prodid">Product ID</label>
     	<input type="text" name="prodid" id="prodid">
+        <br><br>
     	<label for="ccnum">Credit Card Number</label>
     	<input type="text" name="ccnum" id="ccnum">
+        <br><br>
         <label for="cvv">CVV/CVC</label>
     	<input type="text" name="cvv" id="cvv">
+        <br><br><br>
     	<input type="submit" name="submit" value="Submit">
     </form>
 
