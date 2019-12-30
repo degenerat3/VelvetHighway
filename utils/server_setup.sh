bf5b1b8f4e67d7b727b01e26c7868662805a5afe
degenerat3 <james.maskelony@gmail.com>
2019-12-30 17:06:24 -0500

    make non interactive

--- utils/server_setup.sh
+++ utils/server_setup.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install apache2 php libapache2-mod-php mysql-server
-sudo systemctl restart apache
+sudo DEBIAN_FRONTEND=noninteractive apt -y install apache2 php libapache2-mod-php mysql-server
+sudo systemctl restart apache2
