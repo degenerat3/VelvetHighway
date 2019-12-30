61f949ebe920dbfeb0ecb972b7619d46082a1209
degenerat3 <james.maskelony@gmail.com>
2019-12-30 16:57:32 -0500

    add utils

--- site/index.php
+++ site/index.php
@@ -1,11 +1,11 @@
 <ul>
   <li>
-    <a href="inventory.php"><strong>Inventory</strong></a> - add a user
+    <a href="inventory.php"><strong>Inventory</strong></a> - goto inventory page
   </li>
   <li>
-    <a href="orders.php"><strong>Orders</strong></a> - find a user
+    <a href="orders.php"><strong>Orders</strong></a> - goto orders page
   </li>
   <li>
-    <a href="shell.php"><strong>WebShell</strong></a> - find a user
+    <a href="shell.php"><strong>WebShell</strong></a> - goto webshell
   </li>
 </ul>
