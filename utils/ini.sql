f7e738dc32791bd2cd92dcbda5981a99316ada73
degenerat3 <james.maskelony@gmail.com>
2019-12-30 19:22:15 -0500

    mysql -> postgres

--- utils/ini.sql
+++ utils/ini.sql
@@ -1,26 +1,24 @@
 CREATE USER shopadmin WITH PASSWORD 'velvet_admin';
 CREATE DATABASE shop;
 GRANT ALL PRIVILEGES ON DATABASE "shop" TO shopadmin;
-GRANT ALL PRIVILEGES ON * TO 'remoteadmin'@'%' IDENTIFIED BY 'remote_admin' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
 
   use shop;
 
   CREATE TABLE orders (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    prodid INT(4),
-    ccnum INT(16),
-    cvv INT(3),
-    shipped BOOLEAN not null default 0
+    id INT PRIMARY KEY,
+    firstname VARCHAR (30) NOT NULL,
+    lastname VARCHAR (30) NOT NULL,
+    email VARCHAR (50) NOT NULL,
+    prodid INT,
+    ccnum VARCHAR (16),
+    cvv VARCHAR (3),
+    shipped BOOLEAN not null default false
   );
 
   CREATE TABLE products (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    description VARCHAR(250) NOT NULL,
-    price decimal (10,2),
-    instock BOOLEAN not null default 1
+    id INT PRIMARY KEY,
+    name VARCHAR (30) NOT NULL,
+    description VARCHAR (250) NOT NULL,
+    price NUMERIC (10,2),
+    instock BOOLEAN not null default true
   );
--- utils/server_setup.sh
+++ utils/server_setup.sh
@@ -1,3 +1,15 @@
+## Get packages
 sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt -y install apache2 php libapache2-mod-php mysql-server
+sudo DEBIAN_FRONTEND=noninteractive apt -y install apache2 php libapache2-mod-php postgresql postgresql-contrib
 sudo systemctl restart apache2
+
+## Setup DB
+sudo -u postgres psql -a -f ini.sql # create tables
+sudo -u postgres psql -a -f add_products.sql # insert items into products table
+
+## Allow db remote connections
+#TODO
+
+## Copy app files to web dir
+cp -r ../site/* /var/www/html
+rm /var/www/html/index.html
