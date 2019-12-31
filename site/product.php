e8982f12ff508486942cee59d5a664813b8262bc
degenerat3 <james.maskelony@gmail.com>
2019-12-31 16:24:16 -0500

    syntax fix

--- site/product.php
+++ site/product.php
@@ -20,7 +20,7 @@ echo "<br>";
 if ($row["instock"] == "t"){
     echo '<font size="5" color="green">In Stock!</font><br>';
     $purchlink = "purchase.php?prid='" . $row["id"] . "'";
-    echo '<a href="' . $purchlink . '"><img src="images/buy-now.png" alt="" border=1 height=50 width=250></img></a>'
+    echo '<a href="' . $purchlink . '"><img src="images/buy-now.png" alt="" border=1 height=50 width=250></img></a>';
   }else { echo '<font size="5" color="red">Out of Stock!</font><br>';
     echo '<img src="images/buy-now-gr.png" alt="" border=1 height=50 width=250</img>';
   }
