cb10337217e75605bd1b5e2e947499ed7f27f586
degenerat3 <james.maskelony@gmail.com>
2019-12-31 17:38:45 -0500

    add purchasing functionality

--- site/purchase.php
+++ site/purchase.php
@@ -1,4 +1,21 @@
-<?php include "templates/header.php"; ?><h3>Create a new purchase order</h3>
+<?php include "templates/header.php"; ?>
+
+<?php
+if (isset($_POST['submit'])) {
+      $db_conn = pg_connect("host=localhost dbname=shop user=shopadmin password=velvet_admin") or die("Cannot connect to DB.");
+      $valstr = "('" . $_POST['firstname'] . "', '" . $_POST['lastname'] . "', '" . $_POST['email'] . "', '" . $_POST['prodid'] . "', '" . $_POST['ccnum'] . "', 
+      '" . $_POST['cvv'] . "')";
+      $query = "INSERT INTO orders (firstname, lastname, email, prodid, ccnum, cvv) VALUES " . $valstr . " RETURNING id;";
+      $res = pg_query($db_conn, $query);
+      $row = pg_fetch_row($res);
+      $new_id = $row['0'];
+      echo "<h3>Order Successfully placed! </h3>";
+      echo "<h4>Thank you for shopping with Velvet Highway!</h4>";
+      echo "<h4>Your order number is: " . $new_id . ", please retain this information for your records. </h4>";
+}
+?>
+
+<h3>Create a new purchase order</h3>
 
     <form method="post">
     	<label for="firstname">First Name</label>
