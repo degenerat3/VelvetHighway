abcd5c3374d152ac834191b39a204e38965230fb
degenerat3 <james.maskelony@gmail.com>
2019-12-30 23:04:03 -0500

    add new products

--- utils/add_products.sql
+++ utils/add_products.sql
@@ -1,1 +1,6 @@
-INSERT INTO products VALUES('Stolen NSA Documents','500 random pages of classified material from various NSA hacks that we have performed.', 100.99, 1)
+INSERT INTO products VALUES('Stolen NSA Documents','500 random pages of classified material from various NSA hacks that we have performed.', 99.99, 1);
+INSERT INTO products VALUES('Skimmed Credit Cards','10 Credit Card numbers with associated CVV/CVCs and billing addresses', 4.99, 1);
+INSERT INTO products VALUES('Windows Me 0-day','Unpatched RCE in Windows Me', 0.99, 0);
+INSERT INTO products VALUES('ATM Malware','A malicious software suite that, when launched against ATMs can skim card information, distribute cash, and otherwise control the sysytem', 64.99, 1);
+INSERT INTO products VALUES('Botnet Control','Temporarily take control of our efficient and feature-rich botnet, price is per hour of control', 0.49.99, 1);
+
