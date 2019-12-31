d5ecec7eacbd72a6fee5aec09efbd53ec066a169
degenerat3 <james.maskelony@gmail.com>
2019-12-31 14:37:26 -0500

    fix table display

--- site/inventory.php
+++ site/inventory.php
@@ -1,33 +1,24 @@
 <?php include "templates/header.php"; ?>
     <h3>inventory</h3>
-    <table>
-      <thead>
-<tr>
-  <th>#</th>
-  <th>Thumbnail</th>
-  <th>Item Name</th>
-  <th>Price</th>
-  <th>Status</th>
-  <th>Link</th>
-</tr>
-      </thead>
-      <tbody>
 
 
 <?php
 $db_conn = pg_connect("host=localhost dbname=shop user=shopadmin password=velvet_admin") or die("Cannot connect to DB.");
 $query = "SELECT * FROM products";
 $rs = pg_query($db_conn, $query) or die("Cannot execute query: $query\n");
+$result = pg_fetch_all($rs);
+
+echo "<table>\n<thead>\n<tr>\n<th>#</th>\n<th>Name</th>\n<th>Price</th>\n<th>Instock?</th>\n</tr>\n</thead>\n<tbody>\n";
+foreach ($result as $row){
+  echo "<tr>";
+  echo "<td>" . $row["id"] . "</td>";
+  echo "<td>" . $row["name"] . "</td>";
+  echo "<td>" . $row["price"] . "</td>";
+  echo "<td>" . $row["instock"] . "</td>";
+  echo "</tr>";
+}
+echo "</tbody> </table>";
 ?>
 
-<?php while ($row = pg_fetch_row($rs)) { ?>
-      <tr>
-<td><?php echo escape($row["id"]); ?></td>
-<td><?php echo escape($row["name"]); ?></td>
-<td><?php echo escape($row["price"]); ?></td>
-<td><?php echo escape($row["instock"]); ?></td>
-      </tr>
-    <?php } ?>
-      </tbody>
-  </table>
+
 <?php include "templates/footer.php"; ?>
