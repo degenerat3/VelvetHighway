431aa6561098146d26cc63569fc92fc1682c5a3a
degenerat3 <james.maskelony@gmail.com>
2020-01-01 00:49:34 -0500

    fix formatting

--- site/index.php
+++ site/index.php
@@ -1,5 +1,8 @@
 <?php include "templates/header.php"; ?>
+<br>
+<center>
 <img src="images/welcome-page.png" alt="" border=1></img>
 <br>
 <a href="inventory.php"><img src="images/view-inventory.png" alt="" border=1 height=100 width=500></img></a>
+</center>
 <?php include "templates/footer.php"; ?>
--- site/templates/footer.php
+++ site/templates/footer.php
@@ -1,7 +1,9 @@
 <br>
 <br>
 <br>
+<center>
 <p>Disclaimer: This website is purposefully insecure and vulnerable (and also a joke), and as such should not have any real information entered into its forms.</p>
 <p>Note: No listed products are actually available for sale, do not actually attempt to purchase them. </p>
+</center>
 </body>
 </html>
