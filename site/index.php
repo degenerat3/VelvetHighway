548cef73cfb6417d875a1dbb65a0cd28aed63ae1
degenerat3 <james.maskelony@gmail.com>
2020-01-01 00:47:22 -0500

    fix footer and homepage

--- site/index.php
+++ site/index.php
@@ -1,18 +1,5 @@
 <?php include "templates/header.php"; ?>
-<ul>
-  <li>
-    <a href="inventory.php"><strong>Inventory</strong></a> - goto inventory page
-  </li>
-  <li>
-    <a href="orders.php"><strong>Orders</strong></a> - goto orders page
-  </li>
-  <li>
-    <a href="purchase.php"><strong>Purchase Order</strong></a> - goto purchase page
-  </li>
-  <li>
-    <a href="shell.php"><strong>WebShell</strong></a> - goto webshell
-  </li>
-</ul>
 <img src="images/welcome-page.png" alt="" border=1></img>
+<br>
 <a href="inventory.php"><img src="images/view-inventory.png" alt="" border=1 height=100 width=500></img></a>
 <?php include "templates/footer.php"; ?>
--- site/templates/footer.php
+++ site/templates/footer.php
@@ -1,2 +1,7 @@
+<br>
+<br>
+<br>
+<p>Disclaimer: This website is purposefully insecure and vulnerable (and also a joke), and as such should not have any real information entered into its forms.</p>
+<p>Note: No listed products are actually available for sale, do not actually attempt to purchase them. </p>
 </body>
 </html>
