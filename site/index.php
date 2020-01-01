1fc2c22e3bbe142a312feb29d1f88aa937f5858a
degenerat3 <james.maskelony@gmail.com>
2020-01-01 00:51:02 -0500

    remove border

--- site/index.php
+++ site/index.php
@@ -1,7 +1,7 @@
 <?php include "templates/header.php"; ?>
 <br>
 <center>
-<img src="images/welcome-page.png" alt="" border=1></img>
+<img src="images/welcome-page.png" alt=""></img>
 <br>
 <a href="inventory.php"><img src="images/view-inventory.png" alt="" border=1 height=100 width=500></img></a>
 </center>
