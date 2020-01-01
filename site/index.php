628e2b6a575f06a62d80275f41f20cb64009a44a
degenerat3 <james.maskelony@gmail.com>
2020-01-01 00:40:00 -0500

    fix index

--- site/index.php
+++ site/index.php
@@ -13,4 +13,6 @@
     <a href="shell.php"><strong>WebShell</strong></a> - goto webshell
   </li>
 </ul>
+<img src="images/welcome-page.png" alt="" border=1></img>
+<a href="inventory.php"><img src="images/view-inventory.png" alt="" border=1 height=100 width=500></img></a>
 <?php include "templates/footer.php"; ?>
