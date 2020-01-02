99cf1112fd32542374421bdaa07154dd4ef7f15e
degenerat3 <james.maskelony@gmail.com>
2020-01-02 01:14:26 -0500

    fix footer

--- site/templates/footer.php
+++ site/templates/footer.php
@@ -2,6 +2,9 @@
 <br>
 <br>
 <center>
+<footer style="background-color:gray;background-image:none">
+<hr style="height:1px;border-top:1px;visibility:hidden" />
+</footer> 
 <p>Disclaimer: This website is purposefully insecure and vulnerable (and also a joke), and as such should not have any real information entered into its forms.</p>
 <p>Note: No listed products are actually available for sale, do not actually attempt to purchase them. </p>
 </center>
